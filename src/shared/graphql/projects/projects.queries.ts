4bb87aa1cd75ec56580042b18c5f2e32b5f5dcb4
vladislav.aleynikov <vladislav.aleinikov@innowise.com>
2025-09-29 06:45:11 +0300

    feat: add graphql for projects

---
+++ src/shared/graphql/projects/projects.queries.ts
@@ -0,0 +1,28 @@
+import { gql } from "@apollo/client"
+
+export const GET_PROJECTS = gql`
+  query GetProjects {
+    projects {
+      id
+      name
+      internal_name
+      domain
+      start_date
+      end_date
+    }
+  }
+`;
+
+export const GET_PROJECT = gql`
+  query GetProject($projectId: ID!) {
+    project(projectId: $projectId) {
+      id
+      name
+      domain
+      start_date
+      end_date
+      description
+      environment
+    }
+  }
+`;
