b5928399b3bb012220fa14b0be188499892c2ae2
Vlad Aleynikov <vladislav.aleinikov@innowise.com>
2025-09-29 16:53:00 +0300

    fix: fix users list skeleton

--- src/widgets/users-list/ui/users-list.tsx
+++ src/widgets/users-list/ui/users-list.tsx
@@ -6,8 +6,9 @@ import { Dialog, DialogTrigger } from "@/shared/components/ui/dialog";
 import { Button } from "@/shared/components/ui/button";
 import { UserForm } from "@/features/user-form";
 import { UserRole } from "@/shared/types/cv-graphql";
+import { FCWithSkeleton } from "@/shared/types/fc-with-skeleton";
 
-export const UsersList = async () => {
+export const UsersList: FCWithSkeleton<unknown> = async () => {
   const users = await getUsers();
   const authUser = await getAuthUser();
 
