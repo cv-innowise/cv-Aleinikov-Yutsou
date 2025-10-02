d6c282c181fec429d1fef1c334ff728e0c8a16b6
Vlad Aleynikov <vladislav.aleinikov@innowise.com>
2025-10-02 09:44:16 +0300

    feat: add skill item localization

--- src/entity/skill-item/storybook/skill-item.stories.tsx
+++ src/entity/skill-item/storybook/skill-item.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 import { SkillItem } from "../ui/skill-item";
 import { skillsByCategoriesMock } from "../mocks/skills-by-categories.mock";
 import { Mastery } from "@/shared/types/cv-graphql";
+import { NextIntlClientProvider } from "next-intl";
+import message from "@/i18n/messages/en.json";
 
 const meta = {
   title: "Example/SkillItem",
@@ -20,6 +22,13 @@ export const SkillItemNotEditable: Story = {
     isDisabled: false,
     onChange: () => {},
   },
+  render: (args) => {
+    return (
+      <NextIntlClientProvider messages={message} locale="en">
+        <SkillItem {...args} />
+      </NextIntlClientProvider>
+    );
+  },
 };
 
 export const EditableSkillItem: Story = {
@@ -31,4 +40,11 @@ export const EditableSkillItem: Story = {
     isDisabled: false,
     onChange: () => {},
   },
-}; 
+  render: (args) => {
+    return (
+      <NextIntlClientProvider messages={message} locale="en">
+        <SkillItem {...args} />
+      </NextIntlClientProvider>
+    );
+  },
+};
