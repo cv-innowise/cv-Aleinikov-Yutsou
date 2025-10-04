efbb56b8b69b399915048d42d6b513fab9fc90f5
yutmax <dealplug72@gmail.com>
2025-10-05 01:49:03 +0300

    feat: implement CV edit form

--- src/app/(root)/cvs/[cvId]/page.tsx
+++ src/app/(root)/cvs/[cvId]/page.tsx
@@ -1,3 +1,7 @@
+import { CvEditForm } from "@/features/cv-edit-form/ui/cv-edit-form";
+import { GET_CV } from "@/shared/graphql/cvs/cvs.queries";
+import { CvRequest, CvResponse } from "@/shared/graphql/cvs/cvs.types";
+import { getClient } from "@/shared/lib/apollo/apollo-client";
 import React from "react";
 
 interface CvDetailsPageProps {
@@ -6,10 +10,19 @@ interface CvDetailsPageProps {
 
 const CvDetailsPage: React.FC<CvDetailsPageProps> = async ({ params }) => {
   const { cvId } = await params;
+  const { data } = await getClient().query<CvResponse, CvRequest>({
+    query: GET_CV,
+    variables: { cvId },
+  });
+  const cv = data?.cv;
+
+  if (!cv) {
+    return <div className="text-3xl">CV not found</div>;
+  }
 
   return (
     <>
-      <div className="w-full h-full flex justify-center items-center">CV Details Page - CV ID: {cvId}</div>
+      <CvEditForm cv={cv} />
     </>
   );
 };
---
+++ src/features/cv-edit-form/ui/cv-edit-form.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/shared/components/ui/form";
+import { Cv } from "@/shared/graphql/cvs/cvs.types";
+import { useTranslations } from "next-intl";
+import { useForm } from "react-hook-form";
+import { cvUpdateSchema } from "../validation/cv-update.schema";
+import type { InferType } from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { updateCv } from "@/features/cv-form/mutations/update-cv";
+import { toast } from "sonner";
+import { Input } from "@/shared/components/ui/input";
+import { Textarea } from "@/shared/components/ui/textarea";
+import { Button } from "@/shared/components/ui/button";
+
+interface CvEditFormProps {
+  cv: Cv;
+}
+type FormValues = InferType<typeof cvUpdateSchema>;
+
+export const CvEditForm: React.FC<CvEditFormProps> = ({ cv }) => {
+  const t = useTranslations("cv.editForm");
+  const form = useForm<FormValues>({
+    resolver: yupResolver(cvUpdateSchema),
+    defaultValues: {
+      description: cv.description,
+      education: cv.education,
+      name: cv.name,
+    },
+  });
+
+  const { isDirty } = form.formState;
+
+  const onSubmit = (formData: FormValues) => {
+    updateCv({ cvId: cv.id, ...formData })
+      .then(() => {
+        toast.success(t("updateSuccess"));
+        form.reset(formData);
+      })
+      .catch(() => {
+        toast.error(t("updateError"));
+      });
+  };
+  return (
+    <Form {...form}>
+      <form className="space-y-6 max-w-3xl w-full flex flex-col" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>{t("nameLabel")}</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder={t("nameLabel")} {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="education"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>{t("educationLabel")}</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder={t("educationLabel")} {...field} value={field.value ?? ""} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>{t("descriptionLabel")}</FormLabel>
+              <FormControl>
+                <Textarea placeholder={t("descriptionLabel")} {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button disabled={!isDirty} className="self-end-safe" type="submit">
+          {t("submit")}
+        </Button>
+      </form>
+    </Form>
+  );
+};
